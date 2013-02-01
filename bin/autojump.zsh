f7ed9331d99139788d5a81892115145cf49899c4
Jui-Shan Liang <jsliang.tw@gmail.com>
2013-02-01 12:54:47 +0800

    Print file manager on `jo` and `jco`

--- bin/autojump.zsh
+++ bin/autojump.zsh
@@ -76,7 +76,7 @@ function jo {
 
     local new_path="$(autojump ${@})"
     if [ -d "${new_path}" ]; then
-        echo -e "\\033[31m${new_path}\\033[0m"
+        echo -e "\\033[31m${AUTOJUMP_FILEMGR} \"${new_path}\"\\033[0m"
         $AUTOJUMP_FILEMGR "${new_path}"
     else
         echo "autojump: directory '${@}' not found"
